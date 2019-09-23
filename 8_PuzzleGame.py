a71a7ea6b1764b460fcfc8612228e600563ea4b6
allen880612 <allen880612@gmail.com>
2019-09-24 00:29:48 +0800

    8Puzzle Ver 0.0: 3 window switch

---
+++ 8_PuzzleGame.py
@@ -0,0 +1,92 @@
+﻿import sys
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
+from View import MenuWindow
+
+class First(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.btn = QPushButton("to Second", self)
+        self.btn.move(30, 50)
+
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('8 Puzzle! - Initialize')
+        self.show()
+
+
+class Second(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.btn = QPushButton("to third", self)
+        self.btn.move(30, 50)
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('8 puzzle - Beginning')
+
+
+class Third(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.btn = QPushButton("to First", self)
+        self.btn.move(30, 50)
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('8 puzzle - Gaming')
+
+
+def showAnotherWindow(openIndex, closeIndex):
+    GameStates[openIndex].show()
+    GameStates[closeIndex].hide()
+
+if __name__ == '__main__':
+    GameStates = []
+    gameState = 0
+    # app = QApplication(sys.argv)
+    # Gamestate.append(MenuForm.Menu())
+    # Gamestate.append(Second())
+    # Gamestate.append(Third())
+    # Gamestate[0].show()
+    # Gamestate[0].btn.clicked.connect(lambda: showAnotherWindow(1, 0))
+    # Gamestate[1].btn.clicked.connect(lambda: showAnotherWindow(2, 1))
+    # Gamestate[2].btn.clicked.connect(lambda: showAnotherWindow(0, 2))
+
+    app = QApplication(sys.argv)
+    GameStates = [QMainWindow() for i in range(3)]   # 3個遊戲視窗 type = PyQt5.QtWidgets.QMainWindow
+    menuWindow = MenuWindow.Menu()
+    menuWindow.setupUi(GameStates[0])
+    # Choose Begin Block Window
+    GameStates[1] = Second()
+    # Gaming Window
+    GameStates[2] = Third()
+
+    GameStates[0].show()
+    menuWindow.buttonStart.clicked.connect(lambda: showAnotherWindow(1, 0))
+    GameStates[1].btn.clicked.connect(lambda: showAnotherWindow(2, 1))
+    GameStates[2].btn.clicked.connect(lambda: showAnotherWindow(0, 2))
+    # while True:
+    #     if gameState == 0 and (not menuWindow.IsStart()) and (not menuWindow.isOpen):
+    #         GameState[0].show()
+    #         menuWindow.isOpen = True;
+    #         # GameState[2].hide()
+    #         print("I am a joke og you?")
+    #     elif gameState == 0 and menuWindow.isOpen:
+    #         menuWindow.isOpen = False
+    #         GameState[1].show()
+    #         GameState[0].hide()
+    #         gameState = 1
+    #         print(menuWindow.isOpen)
+
+    sys.exit(app.exec_())
+
+
+
---
+++ View/MenuWindow.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MenuWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Menu(object):
+
+    def __init__(self):
+        self._isStart = False
+        self.isOpen = False
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lableGameLogo = QtWidgets.QLabel(self.centralwidget)
+        self.lableGameLogo.setGeometry(QtCore.QRect(20, 20, 350, 250))
+        self.lableGameLogo.setObjectName("lableGameLogo")
+        self.textBoxPuzzleSize = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.textBoxPuzzleSize.setGeometry(QtCore.QRect(220, 390, 101, 31))
+        self.textBoxPuzzleSize.setObjectName("textBoxPuzzleSize")
+        self.buttonStart = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonStart.setGeometry(QtCore.QRect(360, 320, 230, 100))
+        self.buttonStart.setObjectName("buttonStart")
+        self.buttonImportImage = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonImportImage.setGeometry(QtCore.QRect(510, 250, 81, 31))
+        self.buttonImportImage.setObjectName("buttonImportImage")
+        self.labelPreviewImage = QtWidgets.QLabel(self.centralwidget)
+        self.labelPreviewImage.setGeometry(QtCore.QRect(420, 50, 180, 180))
+        self.labelPreviewImage.setObjectName("labelPreviewImage")
+        self.labelTip = QtWidgets.QLabel(self.centralwidget)
+        self.labelTip.setGeometry(QtCore.QRect(20, 400, 161, 21))
+        self.labelTip.setObjectName("labelTip")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 21))
+        self.menubar.setObjectName("menubar")
+        self.menu8_Puzzle_Initilize = QtWidgets.QMenu(self.menubar)
+        self.menu8_Puzzle_Initilize.setObjectName("menu8_Puzzle_Initilize")
+        self.menuRecord = QtWidgets.QMenu(self.menubar)
+        self.menuRecord.setObjectName("menuRecord")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menu8_Puzzle_Initilize.menuAction())
+        self.menubar.addAction(self.menuRecord.menuAction())
+
+        #  自定義功能區
+        self.buttonStart.clicked.connect(self.Start)
+        #
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.lableGameLogo.setText(_translate("MainWindow", "Game cover image"))
+        self.buttonStart.setText(_translate("MainWindow", "開始遊戲"))
+        self.buttonImportImage.setText(_translate("MainWindow", "匯入圖片"))
+        self.labelPreviewImage.setText(_translate("MainWindow", "Preview of image"))
+        self.labelTip.setText(_translate("MainWindow", "請輸入圖片切成一列有幾張"))
+        self.menu8_Puzzle_Initilize.setTitle(_translate("MainWindow", "File"))
+        self.menuRecord.setTitle(_translate("MainWindow", "Record"))
+
+    def IsStart(self):
+        return self._isStart
+
+    def Start(self):
+        self._isStart = True
+
