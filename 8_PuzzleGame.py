a647bc2019a6c4fb70951ae00592a6cd641278e2
allen880612 <allen880612@gmail.com>
2019-09-24 00:35:36 +0800

    8Puzzle Ver 0.1: delete useless comment

--- 8_PuzzleGame.py
+++ 8_PuzzleGame.py
@@ -51,14 +51,6 @@ def showAnotherWindow(openIndex, closeIndex):
 if __name__ == '__main__':
     GameStates = []
     gameState = 0
-    # app = QApplication(sys.argv)
-    # Gamestate.append(MenuForm.Menu())
-    # Gamestate.append(Second())
-    # Gamestate.append(Third())
-    # Gamestate[0].show()
-    # Gamestate[0].btn.clicked.connect(lambda: showAnotherWindow(1, 0))
-    # Gamestate[1].btn.clicked.connect(lambda: showAnotherWindow(2, 1))
-    # Gamestate[2].btn.clicked.connect(lambda: showAnotherWindow(0, 2))
 
     app = QApplication(sys.argv)
     GameStates = [QMainWindow() for i in range(3)]   # 3個遊戲視窗 type = PyQt5.QtWidgets.QMainWindow
@@ -73,18 +65,6 @@ if __name__ == '__main__':
     menuWindow.buttonStart.clicked.connect(lambda: showAnotherWindow(1, 0))
     GameStates[1].btn.clicked.connect(lambda: showAnotherWindow(2, 1))
     GameStates[2].btn.clicked.connect(lambda: showAnotherWindow(0, 2))
-    # while True:
-    #     if gameState == 0 and (not menuWindow.IsStart()) and (not menuWindow.isOpen):
-    #         GameState[0].show()
-    #         menuWindow.isOpen = True;
-    #         # GameState[2].hide()
-    #         print("I am a joke og you?")
-    #     elif gameState == 0 and menuWindow.isOpen:
-    #         menuWindow.isOpen = False
-    #         GameState[1].show()
-    #         GameState[0].hide()
-    #         gameState = 1
-    #         print(menuWindow.isOpen)
 
     sys.exit(app.exec_())
 
