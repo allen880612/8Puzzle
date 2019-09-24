14eb0c69306bedba3b32fc1ca5af581247ece271
Raven9527 <acd667y@gmail.com>
2019-09-24 17:14:55 +0800

    8Puzzle Ver 0.8: Fuck you MVC update InitialWindow ui

--- Model/DataModel.py
+++ Model/DataModel.py
@@ -10,11 +10,18 @@ class Signal(QtCore.QObject):
 
 class DataModel():
     def __init__(self):
-        self.rowButtonCount = 0
-        self.dataSignal = Signal()
+        self.rowButtonCount = 0 # 每一列的button個數
+        self.nowNullButtonIndex = 0 # 空格按鈕的索引
+        self.dataSignal = Signal() # 毫無反應 只是個信號
 
     def SetButtonCount(self, count):
         self.rowButtonCount = count
 
     def GetButtonCount(self):
         return self.rowButtonCount
+
+    def SetNowNullButtonIndex(self, btnIndex):
+        self.nowNullButtonIndex = btnIndex
+
+    def GetNowNullButtonIndex(self):
+        return self.nowNullButtonIndex
